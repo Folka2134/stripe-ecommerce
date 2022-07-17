cbc67d2f050b880266a7535c4d50918487a75a8d
Folka2134 <folka2134@gmail.com>
2022-07-17 14:06:29 +0100

    Created featuredProducts populated with filtered products

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,35 @@ import { ShoppingCart } from "./pages/ShoppingCart/ShoppingCart";
 
 const App = () => {
   const [products, setProducts] = useState([])
+  const featuredProducts = []
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list()
     setProducts(data)
   }
 
+  const fetchFeaturedProducts = () => {
+    for (let i = 0; i < products.length; i++) {
+      for (let j = 0; j < products[i].categories.length; j++) {
+        if (products[i].categories[j].name === "featured") {
+          featuredProducts.push(products[i])
+        }
+      }
+    }
+  }
+
   useEffect(() => {
     fetchProducts()
   }, []);
+
+  fetchFeaturedProducts()
 
   return (
     <Router>
       <div>
         <NavBar />
         <Routes>
-          <Route exact path="/" element={<HomePage products={products} />}>
+          <Route exact path="/" element={<HomePage products={products} featuredProducts={featuredProducts} />}>
           </Route>
           <Route path="/shoppingcart" element={<ShoppingCart />}>
           </Route>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { ContentContainer } from '../../components/ContentContainer'
 import { ProductList } from '../../pages/HomePage/ProductList'
 
-export const HomePage = ({ products }) => {
-  // console.log(products);
+export const HomePage = ({ products, featuredProducts }) => {
   return (
     <div className='px-12 h-screen w-full bg-slate-100'>
       <ContentContainer title="Products" children={<ProductList products={products} />} />
-      {/* <ContentContainer title="Featured Products" children={<ProductList products={products.featru} />} /> */}
+      <ContentContainer title="Featured Products" children={<ProductList products={featuredProducts} />} />
     </div>
   )
 }
